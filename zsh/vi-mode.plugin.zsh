a936cbc269a5101b9ad08875d592e75716d439e7
bohrasd <bohrasd@live.cn>
2021-03-07 23:07:17 +0800

    vi mode zsh

--- zsh/vi-mode.plugin.zsh
+++ zsh/vi-mode.plugin.zsh
@@ -78,10 +78,6 @@ bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
 
-# allow ctrl-r and ctrl-s to search the history
-bindkey '^r' history-incremental-search-backward
-bindkey '^s' history-incremental-search-forward
-
 # allow ctrl-a and ctrl-e to move to beginning/end of line
 bindkey '^a' beginning-of-line
 bindkey '^e' end-of-line
