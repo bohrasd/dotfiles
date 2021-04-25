b7e0e57797c426d5471b6831e767fbf5e32bb493
bohrasd <bohrasdf@gmail.com>
2021-04-25 22:43:38 +0800

    nix initialize bash

--- install-nix.sh
+++ install-nix.sh
@@ -1,6 +1,21 @@
+#lxc config set penguin security.nesting true
+#sh <(curl -L https://nixos.org/nix/install) --no-daemon
+. $HOME/.nix-profile/etc/profile.d/nix.sh
 nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 nix-env -iA cachix -f https://cachix.org/api/v1/install
 nix-shell '<home-manager>' -A install
 cachix use nixpkgs
+
+if [[ -d /opt/google/cros-containers/ ]]; then
+  mkdir -p ~/.config/systemd/user/cros-garcon.service.d/
+  cat > ~/.config/systemd/user/cros-garcon.service.d/override.conf <<EOF
+[Service]
+Environment="PATH=%h/.nix-profile/bin:/usr/local/sbin:/usr/local/bin:/usr/local/games:/usr/sbin:/usr/bin:/usr/games:/sbin:/bin"
+Environment="XDG_DATA_DIRS=%h/.nix-profile/share:%h/.local/share:/usr/local/share:/usr/share"
+EOF
+fi
+
+cp ./home.nix ~/.config/nixpkgs/home.nix
+home-manager switch
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+if [[ -d /opt/google/cros-containers/ ]]; then
+  echo "true"
+fi
